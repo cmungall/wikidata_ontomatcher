d5e36c0ced684d73864a8014b5685f8fecbc3c83
cmungall <cjm@berkeleybop.org>
2019-11-22 08:34:12 -0800

    Dockerfile for #2

--- install.pl
+++ install.pl
@@ -1,6 +1,9 @@
 
 install_requirements :-
         ensure_loaded(pack),
+        % https://swi-prolog.discourse.group/t/www-swi-prolog-org-goes-https/811/3
+        ensure_loaded(library(prolog_pack)),
+        set_setting(prolog_pack:server, 'https://www.swi-prolog.org/pack/'),
         Opts=[interactive(false)],
         requires(X),
         pack_install(X, Opts),
